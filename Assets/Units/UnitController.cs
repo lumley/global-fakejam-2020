6f28d3961e0d37071d655f74e05759057ef43a0b
PixelBumper <PixelBumper@users.noreply.github.com>
2020-02-08 20:06:15 +0100

    Added health to the units

--- Assets/Units/UnitController.cs
+++ Assets/Units/UnitController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Fakejam.Units;
 using UnityEngine;
 using UnityEngine.AI;
+using Object = UnityEngine.Object;
 
 namespace Units
 {
@@ -9,41 +11,76 @@ namespace Units
     public class UnitController : MonoBehaviour
     {
         [SerializeField] private UnitDefinition unitDefinition;
-        [SerializeField] private GameObject target;
 
         private SphereCollider _attackRangeCollider;
-        private SphereCollider _viewRangeCollider;
 
         private NavMeshAgent _navMeshAgent;
+
+        private int _health;
 
         // Start is called before the first frame update
         void Start()
         {
             _navMeshAgent = GetComponent<NavMeshAgent>();
-            _navMeshAgent.destination = target.transform.position;
             _navMeshAgent.speed = unitDefinition.MovementSpeed;
 
             var parentTransform = transform.parent.gameObject.transform;
             _attackRangeCollider = parentTransform.Find("AttackRange").GetComponent<SphereCollider>();
             _attackRangeCollider.radius = unitDefinition.AttackRange;
 
+            _health = unitDefinition.MaxHealth;
         }
 
-        void Attack(GameObject[] targets)
+        void Attack(IEnumerable<GameObject> targets)
         {
-            foreach (GameObject target in targets)
+            if (gameObject.activeSelf == false)
             {
-                Debug.Log("start attacking " + target.name);    
+                Debug.Log("Unit cannot attack as it is already dead");
+                return;
             }
 
-            
+            foreach (GameObject target in targets)
+            {
+                var unitController = target.GetComponent<UnitController>();
+                if (unitController == null)
+                {
+                    Debug.Log("tried to attack non attackable object '" + target.name + "'");
+                    continue;
+                }
+
+                unitController.TakeDamage(unitDefinition.Damage);
+                Debug.Log("attacking " + target.name);
+            }
+        }
+
+        void TakeDamage(int damage)
+        {
+            if (gameObject.activeSelf == false)
+            {
+                Debug.Log("Unit cannot take damage as it is already dead");
+                return;
+            }
+
+            _health -= damage;
+            var objectName = gameObject.name;
+            Debug.Log("Unit '" + objectName + " took " + damage + "damage");
+            if (_health <= 0)
+            {
+                Debug.Log("Unit '" + objectName + "' died");
+                gameObject.SetActive(false);
+            }
         }
 
         void Move(Vector3 position)
         {
+            if (gameObject.activeSelf == false)
+            {
+                Debug.Log("Unit cannot move as it is already dead");
+                return;
+            }
+
             Debug.Log("Move to position " + position);
             _navMeshAgent.destination = position;
         }
-
     }
 }
