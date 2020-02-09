f47300586f2cd28702714b0bef174ca1dbb6a759
Sergio R. Lumley <lumley256@gmail.com>
2020-02-09 12:45:21 +0100

    Fixed unit prefabs

--- Assets/Units/Billboard.cs
+++ Assets/Units/Billboard.cs
@@ -7,36 +7,21 @@ namespace Units
     public class Billboard : MonoBehaviour
     {
         [SerializeField]
-        private Camera camera;
+        private Camera _camera;
         
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
-
-            if (camera == null)
+            if (_camera == null)
             {
-                camera = Camera.main;
+                _camera = Camera.main;
             }
-
-            transform.LookAt(camera.transform.position, Vector3.up);
-            //transform.forward = Camera.main.transform.forward;
-        }
-
-        private void Awake()
-        {
-            if (camera == null)
+            
+            if (_camera == null)
             {
-                camera = Camera.main;
+                return;
             }
-
-            transform.LookAt(camera.transform.position, Vector3.up);
-            //if (Camera.main != null) transform.LookAt(Camera.main.transform.position, Vector3.up);
-        }
-
-
-        // Update is called once per frame
-        void Update()
-        {
+            transform.LookAt(_camera.transform.position, Vector3.up);
         }
     }
 }
