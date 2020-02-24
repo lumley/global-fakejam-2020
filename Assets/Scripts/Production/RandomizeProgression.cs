265eee37e0eae346f3b24bde7a5b46d7b0da3d4e
Andy Michael <andy.michael@freiheit.com>
2020-02-24 19:06:09 +0100

    Adding random battles

---
+++ Assets/Scripts/Production/RandomizeProgression.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using Fakejam.Input;
+using Fakejam.Players;
+using Fakejam.Units;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Fakejam.Production
+{
+
+    public class RandomizeProgression : MonoBehaviour
+    {
+        public UnityEvent OnRandomizeProgression;
+
+        public void RandomizeIt()
+        {
+            var progressionManager = Toolbox.Get<ProgressionManager>();
+            progressionManager.Restart();
+
+            var battleManager = Toolbox.Get<BattleManager>();
+
+            var allSquads = new List<Squad>();
+            foreach (UnitDefinition unit in battleManager.AllUnitDefinitions)
+            {
+                allSquads.Add(CreateSquadFor(PlayerType.Enemy1, unit));
+                allSquads.Add(CreateSquadFor(PlayerType.Player, unit));
+            }
+
+            battleManager.SetSquadsForCombat(allSquads);
+
+            OnRandomizeProgression?.Invoke();
+        }
+
+        public Squad CreateSquadFor(PlayerType ownerType, UnitDefinition unit)
+        {
+            return new Squad
+            {
+                Owner = ownerType,
+                UnitDefinition = unit,
+                Count = Random.Range(0, unit.SquadSize * 2)
+            };
+        }
+
+    }
+}
+
+    
